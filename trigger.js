dbc0ee6049a689319a2230c691167243e2889fa6
m-lel-m <134222393+m-lel-m@users.noreply.github.com>
2023-06-05 10:52:18 -0700

    Update trigger.js

--- trigger.js
+++ trigger.js
@@ -1,14 +1,17 @@
 nexusclient.sys.trigger = function(c) {
-	var res;
-	if (c === "You have recovered your balance.") nexusclient.sys.onBal();
-	else if (c.includes("You have slain")) {
+	if (c === "You have recovered your balance.") {
+		nexusclient.sys.onBal(); 
+	}
+	if (c.includes("You have slain")) {
 		nexusclient.sys.onKill();
 	}
-	else if (nexusclient.sys.isInterruptLine(c)) {
+	if (nexusclient.sys.isInterruptLine(c)) {
 		nexusclient.sys.send("ih");
-	} else if (c.includes("You collapse to the ground, killed")) {
+	}
+	if (c.includes("You collapse to the ground, killed")) {
 		nexusclient.sys.onDeath();
-	} else if (c === "You can once again embrace the surrounding energies.") {
+	}
+	if (c === "You can once again embrace the surrounding energies.") {
 		nexusclient.sys.onHealBalGained();
 	} 
 	return false;

0e106ab9c2346eb729ed9e1da9952cfac5d29998
m-lel-m <134222393+m-lel-m@users.noreply.github.com>
2023-06-04 22:23:17 -0700

    Update gmcp.js

--- gmcp.js
+++ gmcp.js
@@ -1,22 +1,22 @@
-client.sys.gmcp = function(m, r) {
+nexusclient.sys.gmcp = function(m, r) {
 	if (m === "Char.Vitals") {
-		client.sys.systems.forEach(function(sys){
-			client.sys.sys.health[sys] = parseFloat(r[sys]);
-			client.sys.sys.efficacy[sys] = parseFloat(r[sys+"_efficacy"]);
+		nexusclient.sys.systems.forEach(function(sys){
+			nexusclient.sys.sys.health[sys] = parseFloat(r[sys]);
+			nexusclient.sys.sys.efficacy[sys] = parseFloat(r[sys+"_efficacy"]);
 		});
-		client.sys.bal = r.bal == "1" ? true : false;
-		client.sys.wwBal = r.ww == "1" ? true : false;
-		client.sys.hpperc = parseInt(r.hp)/parseInt(r.maxhp);
-		client.sys.class = r.class;
-		client.sys.prios = r.ww_prios;
-		eval("client.sys.cooldowns = {"+r.cooldowns+"}");
-		client.sys.sanity = r.sa;
-		client.sys.nanites = r.nn;
+		nexusclient.sys.bal = r.bal == "1" ? true : false;
+		nexusclient.sys.wwBal = r.ww == "1" ? true : false;
+		nexusclient.sys.hpperc = parseInt(r.hp)/parseInt(r.maxhp);
+		nexusclient.sys.class = r.class;
+		nexusclient.sys.prios = r.ww_prios;
+		eval("nexusclient.sys.cooldowns = {"+r.cooldowns+"}");
+		nexusclient.sys.sanity = r.sa;
+		nexusclient.sys.nanites = r.nn;
 	} else if (m === "Char.Defences.List") {
-		client.sys.defslist = [];
-		for (var i of r) { client.sys.defslist.push(i.desc); }
+		nexusclient.sys.defslist = [];
+		for (var i of r) { nexusclient.sys.defslist.push(i.desc); }
 	} else if (m === "IRE.Target.Info") {
-		client.sys.tarHealth = parseInt(r.hpperc.replace("%",""))
+		nexusclient.sys.tarHealth = parseInt(r.hpperc.replace("%",""))
 	}
 	return false;	
 }

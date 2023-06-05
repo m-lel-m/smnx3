e5cb030bac5657cc05b2b2cf35534c82b68cde69
m-lel-m <134222393+m-lel-m@users.noreply.github.com>
2023-06-04 22:33:08 -0700

    Update defup.js

--- defup.js
+++ defup.js
@@ -1,6 +1,6 @@
 nexusclient.sys.verb_def = function() {
 	for (var d of nexusclient.sys.nanodefs) { 
-		if !nexusclient.sys.defslist.includes(d.name) { nexusclient.sys.send(d.cmd); return; }
+		if (!nexusclient.sys.defslist.includes(d.name)) { nexusclient.sys.send(d.cmd); return; }
 		else { nexusclient.sys.info("all defups complete"); }
 	}
 }

05fa0f6fe75c14312f9fdf827c88a4f9c3d324f6
m-lel-m <134222393+m-lel-m@users.noreply.github.com>
2023-06-13 13:33:52 -0700

    Delete defup.js

--- defup.js
+++
@@ -1,6 +0,0 @@
-nexusclient.sys.verb_def = function() {
-	for (var d of nexusclient.sys.nanodefs) { 
-		if (!nexusclient.sys.defslist.includes(d.name)) { nexusclient.sys.send(d.cmd); return; }
-		else { nexusclient.sys.info("all defups complete"); }
-	}
-}
